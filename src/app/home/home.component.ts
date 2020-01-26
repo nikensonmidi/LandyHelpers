b4fbe9cd7fb564f626e32755f1af514ade158888
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2020-01-26 12:00:15 -0500

    add filter for table

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,8 +11,18 @@ export class HomeComponent implements OnInit {
   rooms: Room[];
   from: number;
   to: number;
-  searchText: string;
+  filteredRooms: Room[];
 
+  private _searchText: string;
+  public get searchText(): string {
+    return this._searchText;
+  }
+  public set searchText(value: string) {
+    this._searchText = value;
+    this.filteredRooms = this._searchText
+      ? this.filterRoomList(this._searchText)
+      : this.rooms;
+  }
   constructor() { }
 
   ngOnInit() {
@@ -25,6 +35,15 @@ export class HomeComponent implements OnInit {
      const tempRoom: Room = new Room(index);
      this.rooms.push(tempRoom);
     }
+    this.filteredRooms = this.rooms;
+    }
+
+    filterRoomList(filter: string): Room[] {
+return this.rooms.filter( r => {
+
+return r.roomNumber.toString().includes(filter.toLowerCase().trim());
+
+});
     }
 
 }
