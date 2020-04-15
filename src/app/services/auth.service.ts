294b5ee5e71bcbfb076f2136f5d20c12cb094235
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2020-04-15 09:26:50 -0400

    fix(Home):auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,57 +2,59 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { auth, firestore } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore';
 import { Observable, of } from 'rxjs';
-import { switchMap} from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-user$: Observable<User>;
-
+  user$: Observable<User>;
+user: User;
   constructor(
     private afAuth: AngularFireAuth,
     private afs: AngularFirestore,
     private router: Router
   ) {
-this.user$ = this.afAuth.authState.pipe(
-  switchMap(user => {
-if (user) {
-  return this.afs.doc<User>(`user/${user.uid}`).valueChanges();
-} else {
-  return of(null);
-}
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
 
-  })
-);
+  async googleSignin() {
+    const provider = new auth.GoogleAuthProvider();
+    //const provider = new auth.EmailAuthProvider();
+    const credential = await this.afAuth.auth.signInWithPopup(provider);
+    this.user = credential.user;
+    return this.updateUserData(credential.user);
+  }
 
-   }
+  async signOut() {
+    await this.afAuth.auth.signOut();
+    return this.router.navigate(['/']);
+  }
 
-async googleSignin() {
-const provider = new auth.GoogleAuthProvider();
-//const provider = new auth.EmailAuthProvider();
-const credential = await this.afAuth.auth.signInWithPopup(provider);
-return this.updateUserData(credential.user);
-}
+  private updateUserData(user: User) {
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(
+      `users/${user.uid}`
+    );
+    const userData = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName
+    };
 
-async signOut() {
-  await this.afAuth.auth.signOut();
-  return this.router.navigate(['/']);
-}
-
-
-private updateUserData(user: User) {
-const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
-const userData = {
-  uid: user.uid,
-  email: user.email,
-  displayName: user.displayName
-};
-
-return userRef.set(userData, { merge: true} );
-}
-
+    return userRef.set(userData, { merge: true });
+  }
 }
