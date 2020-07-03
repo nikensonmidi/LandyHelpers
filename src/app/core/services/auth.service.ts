8fc447c27eeec3442451d34773c49603a27d703f
nikensonmidi@gmail.com <nikensonmidi@gmail.com>
2020-07-03 10:37:00 -0400

    fix(Auth): persistent login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -34,19 +34,30 @@ user: User;
 
   async googleSignin() {
     const provider = new auth.GoogleAuthProvider();
-    //const provider = new auth.EmailAuthProvider();
+    // const provider = new auth.EmailAuthProvider();
     const credential = await this.afAuth.auth.signInWithPopup(provider);
     this.user = credential.user;
+    this.setSessionLogin(true);
     return this.updateUserData(credential.user);
+  }
+
+  private setSessionLogin(isLogin: boolean) {
+    const loginState = isLogin ? 'true' : 'false';
+    sessionStorage.setItem('islogin', loginState);
+    if (isLogin) { sessionStorage.setItem('user', JSON.stringify(this.user)); }
+
   }
 
   async signOut() {
     await this.afAuth.auth.signOut();
     this.user = null;
+    this.setSessionLogin(false);
     return this.router.navigate(['/']);
   }
  userIsAuthenticated(): boolean {
-   return this.user !== null && this.user !== undefined;
+const isLogin = sessionStorage.getItem('islogin');
+this.user = /true/.test(isLogin) ? JSON.parse( sessionStorage.getItem('user')) as User : this.user;
+return this.user !== null && this.user !== undefined;
  }
   private updateUserData(user: User) {
     const userRef: AngularFirestoreDocument<User> = this.afs.doc(
